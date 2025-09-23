0ef6a4f67dcfa1406468dc1bb5a2997422ea239c
Garfield <gnguyengia@pay.com.au>
2025-09-23 22:50:56 +0700

    chore: basic project setup

---
+++ modules/welcome/branding/index.tsx
@@ -0,0 +1,11 @@
+import { AnimatePresence } from "framer-motion";
+
+export function Branding() {
+    return (
+        <section className="w-[100vw] h-[100px]">
+            <AnimatePresence>
+                
+            </AnimatePresence>
+        </section>
+    )
+}
---
+++ modules/welcome/index.tsx
@@ -0,0 +1,8 @@
+
+export function Welcome() {
+    return (
+        <main className="flex item-center justify-center pt-16 pb-4">
+            PLACEHOLDER
+        </main>
+    )
+}
