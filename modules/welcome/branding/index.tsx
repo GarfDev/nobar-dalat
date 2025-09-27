b449613a0428b3eaec3ff12d7f4a61a6a44a55f7
Garfield <gnguyengia@pay.com.au>
2025-09-28 00:09:33 +0700

    chore: update a bit responsive

--- modules/welcome/branding/index.tsx
+++ modules/welcome/branding/index.tsx
@@ -75,32 +75,10 @@ export function Branding() {
         <img
           src="/images/nobar-logo-black-white.png"
           alt="nobar-dalat-logo"
-          className="h-[170px]"
+          className="h-[200px] lg:h-[250px]"
         />
       </motion.div>
       {/** END MAIN LOGO TEXT */}
-
-      {/** SEC LOGO TEXT */}
-      {/* <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: {
-            delay: LOGO_DELAY,
-            duration: 1,
-          },
-        }}
-        className="absolute top-0 bottom-0 left-0 right-0 m-[auto] w-fit h-fit flex flex-col items-center justify-center text-primary-500 z-10"
-      >
-        <img
-          src="/images/nobar-logo-color.png"
-          alt="nobar-dalat-logo"
-          className="h-[170px]"
-        />
-      </motion.div> */}
-      {/** END SEC LOGO TEXT */}
-
-      {/** FLOATING COMPONENTS */}
 
       <motion.div
         initial={{ transform: "translateX(100px)" }}
@@ -129,6 +107,22 @@ export function Branding() {
       >
         <Link to="concept" smooth duration={500}>
           <h3 className="text-2xl">CONCEPT</h3>
+        </Link>
+      </motion.div>
+
+      <motion.div
+        initial={{ transform: "translateX(-150px)" }}
+        animate={{
+          transform: "translateY(0px)",
+          transition: {
+            delay: LOGO_DELAY,
+            duration: 0.3,
+          },
+        }}
+        className="absolute bottom-5 left-5 z-10"
+      >
+        <Link to="menu" smooth duration={500}>
+          <h3 className="text-2xl">OUR MENU</h3>
         </Link>
       </motion.div>
     </section>
