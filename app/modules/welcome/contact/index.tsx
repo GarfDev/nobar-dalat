05c7f1ec9a9eb5a6f572ef8e30e94cceb662cb7c
Garfield <gnguyengia@pay.com.au>
2025-09-29 08:10:41 +0700

    feat: add backdrop blur for all section

--- app/modules/welcome/contact/index.tsx
+++ app/modules/welcome/contact/index.tsx
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <motion.section
       id="concept"
-      className="h-[20vh] w-[100vw] bg-[#000000da] flex shadow-[0_-25px_50px_-12px_rgb(0,0,0,0.25)] gap-10"
+      className="h-[20vh] w-[100vw] bg-[#000000da] backdrop-blur-md flex shadow-[0_-25px_50px_-12px_rgb(0,0,0,0.25)] gap-10"
     >
       Day la cai contact
     </motion.section>
--- app/modules/welcome/map/index.tsx
+++ app/modules/welcome/map/index.tsx
@@ -7,7 +7,7 @@ export function Map() {
   return (
     <motion.section
       id="concept"
-      className="h-[100vh] w-[100vw] bg-[#4d2a65cb] flex shadow-[0_-25px_50px_-12px_rgb(0,0,0,0.25)] gap-10"
+      className="h-[100vh] w-[100vw] bg-[#4d2a65cb] backdrop-blur-md flex shadow-[0_-25px_50px_-12px_rgb(0,0,0,0.25)] gap-10"
     >
       Day la cai map
     </motion.section>
