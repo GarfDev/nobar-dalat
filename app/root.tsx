ca8a623a68c0a9221a21c77fe8a86985e6091f97
Garfield <gnguyengia@pay.com.au>
2026-02-11 13:36:14 +0700

    chore: update google tag manager, add speed analytics

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import type { Route } from "./+types/root";
 import "./app.css";
 import i18next from "./i18n";
@@ -26,6 +28,19 @@ export const links: Route.LinksFunction = () => [
     sizes: "16x16",
     href: "/favicon-16x16.png",
   },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "192x192",
+    href: "/android-chrome-192x192.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "512x512",
+    href: "/android-chrome-512x512.png",
+  },
+  { rel: "icon", href: "/favicon.ico" },
   { rel: "manifest", href: "/site.webmanifest" },
 ];
 
@@ -66,14 +81,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
             `,
           }}
         />
-        {/* Google Tag Manager - REPLACE 'GTM-XXXXXXX' WITH YOUR ACTUAL ID */}
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXXXXX');`,
+})(window,document,'script','dataLayer','GTM-K7T6CHDN');`,
           }}
         />
         <Meta />
@@ -93,6 +108,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {children}
         <ScrollRestoration />
         <Scripts />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
